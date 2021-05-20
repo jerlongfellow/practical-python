64369ec81ad8d2f2352b900edfa21e0c247710c6
Jeremy Longfellow <longfellow@utexas.edu>
2021-05-20 15:50:43 -0700

    Exercise 1.9

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -7,12 +7,14 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 month = 0
-extra = 1000.0
+extra_payment_start_month = 60
+extra_payment_end_month = 108
+extra_payment = 1000.0
 
 while principal > 0:
     payment_with_extra = payment
-    if month < 12:
-        payment_with_extra += extra
+    if extra_payment_start_month <= month < extra_payment_end_month:
+        payment_with_extra += extra_payment
     principal = principal * (1 + rate/12) - payment_with_extra
     total_paid = total_paid + payment_with_extra
     month += 1
