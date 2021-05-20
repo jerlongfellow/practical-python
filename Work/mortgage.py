66cf689087db9d3765026c3e566dc15b4c918da5
Jeremy Longfellow <longfellow@utexas.edu>
2021-05-20 15:45:18 -0700

    Exercise 1.8

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,9 +6,16 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+month = 0
+extra = 1000.0
 
 while principal > 0:
-    principal = principal * (1 + rate/12) - payment
-    total_paid = total_paid + payment
+    payment_with_extra = payment
+    if month < 12:
+        payment_with_extra += extra
+    principal = principal * (1 + rate/12) - payment_with_extra
+    total_paid = total_paid + payment_with_extra
+    month += 1
 
 print('Total paid', total_paid)
+print('Total months', month)
