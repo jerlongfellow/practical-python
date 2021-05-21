39bc67bc203bd2e5fb249602b0a7b78c07e17b73
Jeremy Longfellow <longfellow@utexas.edu>
2021-05-20 17:32:17 -0700

    Exercise 1.33

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,6 +2,7 @@
 #
 # Exercise 1.27
 import csv
+import sys
 
 def portfolio_cost(filename):
     total_cost = 0.00
@@ -17,5 +18,10 @@ def portfolio_cost(filename):
             total_cost += num_shares * share_price
     return total_cost
 
-cost = portfolio_cost('Data/portfolio.csv')
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
 print(f'Total cost {cost:.2f}')
