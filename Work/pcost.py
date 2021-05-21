781bf806fbd084cac6087b456042ae2643092361
Jeremy Longfellow <longfellow@utexas.edu>
2021-05-20 17:13:32 -0700

    Exercise 1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,14 +2,17 @@
 #
 # Exercise 1.27
 
-total_cost = 0.00
 
-with open('Data/portfolio.csv', 'rt') as portfolio_file:
-    next(portfolio_file)
-    for line in portfolio_file:
-        split_line = line.split(',')
-        num_shares = int(split_line[1])
-        share_price = float(split_line[2])
-        total_cost += num_shares * share_price
+def portfolio_cost(filename):
+    total_cost = 0.00
+    with open(filename, 'rt') as portfolio_file:
+        next(portfolio_file)
+        for line in portfolio_file:
+            split_line = line.split(',')
+            num_shares = int(split_line[1])
+            share_price = float(split_line[2])
+            total_cost += num_shares * share_price
+    return total_cost
 
-print(f'Total cost {total_cost:.2f}')
+cost = portfolio_cost('Data/portfolio.csv')
+print(f'Total cost {cost:.2f}')
