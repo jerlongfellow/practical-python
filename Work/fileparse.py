a79e10c1cd3dc86f965e555315d368db308d0799
Jeremy Longfellow <longfellow@utexas.edu>
2021-05-22 16:33:09 -0700

    Exercise 3.5

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -5,7 +5,7 @@
 import csv
 
 
-def parse_csv(filename, select=None):
+def parse_csv(filename, select=None, types=None):
     '''
     Parse a CSV file into a list of records
     '''
@@ -30,6 +30,9 @@ def parse_csv(filename, select=None):
             # Filter the row if specfic columns were selected
             if indices:
                 row = [ row[index] for index in indices ]
+
+            if types:
+                row = [ func(val) for func, val in zip(types, row) ]
 
             # Make a dictionary
             record = dict(zip(headers, row))
