faece811bad00dcd5c0c7ac75fd1db094531333a
Jeremy Longfellow <longfellow@utexas.edu>
2021-05-23 11:01:43 -0700

    Exercise 4.2

--- Work/stock.py
+++ Work/stock.py
@@ -3,3 +3,12 @@ class Stock:
         self.name = name
         self.shares = shares
         self.price = price
+
+    def cost(self):
+        return self.shares * self.price
+
+    def sell(self, sold_shares: int):
+        if self.shares < sold_shares:
+            raise RuntimeError('Cannot sell more shares than are currently held.')
+        else:
+            self.shares -= sold_shares
