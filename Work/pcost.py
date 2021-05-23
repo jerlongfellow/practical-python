47bc7d7c3efbb7f5aaf36b202c25256e42772668
Jeremy Longfellow <longfellow@utexas.edu>
2021-05-22 17:36:23 -0700

    Exercise 3.14

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,23 +1,20 @@
 # pcost.py
 #
 # Exercise 1.27
-import csv
+
+from report import read_portfolio
 import sys
+
 
 def portfolio_cost(filename):
     total_cost = 0.00
-    with open(filename, 'rt') as portfolio_file:
-        rows = csv.reader(portfolio_file)
-        headers = next(rows)
-        for row_number, row in enumerate(rows, start=1):
-            record = dict(zip(headers, row))
-            try:
-                num_shares = int(record['shares'])
-                share_price = float(record['price'])
-                total_cost += num_shares * share_price
-            except ValueError:
-                print(f"Row {row_number}: Couldn't convert: {row}")
+    portfolio = read_portfolio(filename)
+
+    for holding in portfolio:
+        total_cost += holding['shares'] * holding['price']
+
     return total_cost
+
 
 if len(sys.argv) == 2:
     filename = sys.argv[1]
