58a0c079b1c083945dc0d4185bf09849f5d08d59
Jeremy Longfellow <longfellow@utexas.edu>
2021-05-23 12:05:25 -0700

    Exercise 4.6

--- Work/tableformat.py
+++ Work/tableformat.py
@@ -10,3 +10,44 @@ class TableFormatter:
         Emit a single row of table data.
         '''
         raise NotImplementedError()
+
+
+class TextTableFormatter(TableFormatter):
+    '''
+    Emit a table in plain-text format
+    '''
+    def headings(self, headers):
+        for header in headers:
+            print(f'{header:>10s}', end=' ')
+        print()
+        print(('-' * 10 + ' ') * len(headers))
+
+    def row(self, rowdata):
+        for datum in rowdata:
+            print(f'{datum:>10s}', end=' ')
+        print()
+
+
+class CSVTableFormatter(TableFormatter):
+    '''
+    Output portfolio data in CSV format.
+    '''
+    def headings(self, headers):
+        print(','.join(headers))
+
+    def row(self, rowdata):
+        print(','.join(rowdata))
+
+
+class HTMLTableFormatter(TableFormatter):
+    '''
+    Output portfolio data in CSV format.
+    '''
+    def headings(self, headers):
+        print(self.make_table_row(headers, cell_type='h'))
+
+    def row(self, rowdata):
+        print(self.make_table_row(rowdata))
+
+    def make_table_row(self, data, cell_type='d'):
+        return '<tr>' + ''.join([ f'<t{cell_type}>{datum}</t{cell_type}>' for datum in data ]) + '</tr>'
