bacbf91a533b2b3fa7b175d52dba1584d952368d
Jeremy Longfellow <longfellow@utexas.edu>
2021-05-27 15:39:09 -0700

    Exercise 6.12

--- Work/ticker.py
+++ Work/ticker.py
@@ -1,5 +1,14 @@
 from follow import follow
+import tableformat
+import report
 import csv
+
+class Ticker:
+    def __init__(self, tickerdict):
+        self.Name = tickerdict['name']
+        self.Price = tickerdict['price']
+        self.Change = tickerdict['change']
+
 
 def select_columns(rows, indices):
     for row in rows:
@@ -16,6 +25,11 @@ def make_dicts(rows, headers):
         yield dict(zip(headers, row))
 
 
+def make_tickers(tickerdicts):
+    for tickerdict in tickerdicts:
+        yield Ticker(tickerdict)
+
+
 def filter_symbols(rows, names):
     for row in rows:
         if row['name'] in names:
@@ -28,6 +42,15 @@ def parse_stock_data(lines):
     rows = convert_types(rows, [str, float, float])
     rows = make_dicts(rows, ['name', 'price', 'change'])
     return rows
+
+
+def ticker(portfile, logfile, fmt):
+    portfolio = report.read_portfolio(portfile)
+    formatter = tableformat.create_formatter(fmt)
+    rows = parse_stock_data(follow('Data/stocklog.csv'))
+    rows = filter_symbols(rows, portfolio)
+    rows = make_tickers(rows)
+    tableformat.print_table(rows, ['Name', 'Price', 'Change'], formatter)
 
 
 if __name__ == '__main__':
