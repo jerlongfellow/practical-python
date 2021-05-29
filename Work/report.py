689595fddfe1104a5851592a1ae52a78a2244067
Jeremy Longfellow <longfellow@utexas.edu>
2021-05-29 09:43:05 -0700

    Exercise 8.3

--- Work/report.py
+++ Work/report.py
@@ -6,6 +6,13 @@ from fileparse import parse_csv
 from stock import Stock
 from portfolio import Portfolio
 import tableformat
+import logging
+
+logging.basicConfig(
+    filename = 'app.log',        # Name of the log file (omit to use stderr)
+    filemode = 'w',              # File mode (use 'a' to append)
+    level    = logging.WARNING,  # Logging level (DEBUG, INFO, WARNING, ERROR, or CRITICAL)
+)
 
 
 def read_portfolio(filename, **opts):
